e67409f9e04df32ba5cc630705b87ab8de6c4f8c
Harisundar25 <100459035+Harisundar25@users.noreply.github.com>
2024-02-17 18:34:43 +0530

    Console class

---
+++ MyFirstApp/MyFirstApp/Console_class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+       public class Sample
+        {
+            public static void Main()
+            {
+                // print line by line
+                System.Console.WriteLine("Welcome");
+                System.Console.WriteLine("team");
+                // print side by side
+                System.Console.Write("Welcome");
+                System.Console.Write("team");
+
+                // wait for pressing some key on keyboard
+                System.Console.ReadKey();
+
+                //clear screen
+                System.Console.Clear();
+
+                System.Console.WriteLine("Thank you");
+                System.Console.ReadKey();
+
+            }
+       }
+}
