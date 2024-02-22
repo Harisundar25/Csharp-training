95d24a3cafc4c59c9f876f669e8b5617325765a0
HariSundar-GRL <145016863+HariSundar-GRL@users.noreply.github.com>
2024-02-22 19:02:12 +0530

    Naming Conventions

---
+++ Naming Conventions & Conversions/Console class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naming_Conventions___Conversions
+{
+    internal class Console_class
+    {
+        public static void Example()
+        {
+            Console.WriteLine("HELLO WELCOME"); // prints the text and jumps to a new new line.
+            Console.Write("HELLO"); // prints the text in the same line.
+            Console.Write("WELCOME");
+            Console.ReadKey();
+
+            Console.Write("Enter a string and press enter :");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have entered : {0}", readInput);
+            Console.Write("Enter a string and press enter :");
+            Console.Beep(); // Will give a beep sound while enter a key
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is : {0}", asciiValue);
+            Console.ReadKey(); // returns the ASCII value of the given input char
+            
+
+        }
+    }
+}
---
+++ Naming Conventions & Conversions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naming_Conventions___Conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console_class.Example();
+        }
+    }
+}
