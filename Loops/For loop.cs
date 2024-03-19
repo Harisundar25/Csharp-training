816958b5907b99cde8782668fd34097a510bd6d1
HariSundar-GRL <145016863+HariSundar-GRL@users.noreply.github.com>
2024-03-19 19:22:13 +0530

    loop

---
+++ Loops/For loop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class For_loop
+    {
+        public static void Output()
+        {
+            for(int counter = 0; counter < 50;  counter+=5)
+            {
+                Console.WriteLine(counter);
+            }
+            Console.ReadKey();
+        }
+    }
+}
