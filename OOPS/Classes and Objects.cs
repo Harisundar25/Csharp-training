674e69187433674a26dab2568e4bc9d0f6c60edc
Harisundar25 <2k20ece028@kiot.ac.in>
2024-04-03 09:53:30 +0530

    OOPS concept

---
+++ OOPS/Classes and Objects.cs
@@ -0,0 +1,19 @@
+﻿namespace OOPS
+{
+    internal class Classes_and_Objects
+    {
+        // constructor
+        public Classes_and_Objects()
+        {
+            Console.WriteLine("The car was created");
+        }
+        public void Drive()
+        {
+            Console.WriteLine("The car is moving");
+        }
+        public void stop()
+        {
+            Console.WriteLine("The car has stopped");
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace OOPS
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Classes_and_Objects audi = new Classes_and_Objects();
+            audi.Drive();
+            Classes_and_Objects bmw = new Classes_and_Objects();
+
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Press 0 to stop the car");
+            if (userInput == "0")
+            {
+                audi.stop();
+            }
+            else
+            {
+                Console.WriteLine("Car drives indefinitely");
+            }
+        }
+    }
+}
