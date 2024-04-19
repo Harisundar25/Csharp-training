9a65c06460f12ba9f44d366d054998ab448e1e31
HariSundar-GRL <145016863+HariSundar-GRL@users.noreply.github.com>
2024-04-19 15:01:58 +0530

    Custom multiple API implementation

---
+++ CustomAPI/Dependencies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomAPI
+{
+    public class Dependencies
+    {
+        public class ExerFwAPIWrite
+        {
+            public List<string> apiRawData = new List<string>();
+
+            public bool hexFlag = false;
+
+            public string fwResponse = "Success";
+
+            public string fwResTooltip = "No Error";
+
+            public int CurrIndex = 0;
+
+            public bool IsSend = false;
+
+        }
+    }
+}
--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -7,28 +7,27 @@ namespace OOPS
     {
         public static void Main(string[] args)
         {
-            //Classes_and_Objects audi = new Classes_and_Objects("Audi A5", 1500, "Red");
-            //audi.Drive();
-            //audi.Details();
+            Classes_and_Objects audi = new Classes_and_Objects("Audi A5", 1500, "Red");
+            audi.Drive();
+            audi.Details();
 
-            //Classes_and_Objects bmw = new Classes_and_Objects("BMW M4", 2000);
-            //bmw.Drive();
-            //bmw.Details();
+            Classes_and_Objects bmw = new Classes_and_Objects("BMW M4", 2000);
+            bmw.Drive();
+            bmw.Details();
 
-            //string userInput = Console.ReadLine();
-            //Console.WriteLine("Press 0 to stop the car");
-            //if (userInput == "0")
-            //{
-            //    audi.stop();
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Car drives indefinitely");
-
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Press 0 to stop the car");
+            if (userInput == "0")
+            {
+                audi.stop();
+            }
+            else
+            {
+                Console.WriteLine("Car drives indefinitely");
+            }
 
             Phone Samsung = new Phone();
-            Phone.Introduce();
-            
+
         }
 
     }
