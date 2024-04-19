196d81646b767f6b61cdc55adf54a4dbc67efe26
HariSundar-GRL <145016863+HariSundar-GRL@users.noreply.github.com>
2024-04-19 16:06:34 +0530

    Manual testing

--- CustomAPI/WriteAPI.cs
+++ CustomAPI/WriteAPI.cs
@@ -15,7 +15,11 @@ namespace CustomAPI
         {
             try
             {
-                if (apiRawData.CurrIndex == 0)
+                ExerFwAPIWrite manualApiRawData = new ExerFwAPIWrite();
+                manualApiRawData.apiRawData.Add("0x00,0x01,0x02");
+                manualApiRawData.CurrIndex = 0;
+
+                if (manualApiRawData.CurrIndex == 0)
                     Delay = 100;//Reset the delay
 
                 //apiRawData.CurrIndex: Send button to initiate one API at a time from a group of APIs
@@ -26,62 +30,80 @@ namespace CustomAPI
                 }
                 for (int i = apiRawData.CurrIndex; i < StopIndex; i++)
                 {
-                    string apiString = apiRawData.apiRawData[i];//Taking the input string value
+                    string apiString = manualApiRawData.apiRawData[i];//Taking the input string value
                     bool hexFlag = apiRawData.hexFlag;//Taking the input bool value
                     bool error = false;//initially no errors
 
-                    //Check for ":" in apiString.
+                    //Check for ";" in apiString and update the delay value
                     string[] APIandDelay = apiString.Split(';');
                     if (APIandDelay.Length > 1)
                     {
                         Delay = int.Parse(APIandDelay[1].Trim());
                     }
 
-                    if (hexFlag)//for hex formats (if the hex checkbox was checked then condition will execute)
+                    //if (hexFlag)//for hex formats (if the hex checkbox was checked then condition will execute)
+                    //{
+                    List<byte> inputBytes = new List<byte>();
+                    string byteArr = "";
+                    string[] HexValues = APIandDelay[0].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);//Separating string by commas and empty space 
+                    foreach (string hexValue in HexValues)
                     {
-                        List<byte> inputBytes = new List<byte>();
-                        string byteArr = "";
-                        string[] HexValues = APIandDelay[0].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);//Separating string by commas and empty space 
-                        foreach (string hexValue in HexValues)
+                        try
                         {
-                            try
+                            //Use cases
+                            //1. ,,,, = [,,,,]
+                            //2. ,1,1,2,0 = [1,1,2,0] 
+                            //3. 0x01,0x0,0x1 
+
+                            string lhexValue = hexValue.Trim();
+                            if (lhexValue.Length > 0)
                             {
-                                string data = hexValue.Substring(2);
-                                if (byte.TryParse(data.Trim('x', 'X'), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out byte value))//converting each input hex value to byte
+                                lhexValue = lhexValue.Replace("0x", "");
+                                lhexValue = lhexValue.Replace("0X", "");
+                                if (byte.TryParse(lhexValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out byte res))
                                 {
-                                    inputBytes.Add(value);
-                                }
-                                else
-                                {
-                                    error = true;//if error occurs setting to true
-                                    string errorMessage = "Hexadecimal value is in improper format or input value is higher range for byte "; // this message will print in UI(there is one icon in UI while hover it)
-                                    Console.WriteLine(errorMessage);
-                                    // FWAppObj.FWHost.WriteToDebugLogger(DebugModeType.DEBUG, errorMessage);//Also prints in debug logger
-                                    apiRawData.fwResTooltip = errorMessage; // Adds the Error content to the icon
+                                    inputBytes.Add(res);
                                 }
                             }
-                            catch (Exception ex)
+                            else
+                                continue;
+                            //Trim 0x and 0X
+                            //Trim 
+                            // string data = hexValue.Substring(2);//Can cause exception if length is less than 3
+                            //if (byte.TryParse(data.Trim('x', 'X'), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out byte value))//converting each input hex value to byte
+                            //{
+                            //    inputBytes.Add(value);
+                            //}
                             {
-                                Console.WriteLine(MethodBase.GetCurrentMethod().Name, ex);
+                                error = true;//if error occurs setting to true
+                                string errorMessage = "Hexadecimal value is in improper format or input value is higher range for byte "; // this message will print in UI(there is one icon in UI while hover it)
+                                Console.WriteLine(errorMessage);
+                                // FWAppObj.FWHost.WriteToDebugLogger(DebugModeType.DEBUG, errorMessage);//Also prints in debug logger
+                                apiRawData.fwResTooltip = errorMessage; // Adds the Error content to the icon
                             }
                         }
-                        foreach (byte byteValue in inputBytes)
+                        catch (Exception ex)
                         {
-                            byteArr += (byteValue.ToString() + " ");// all the bytes are comverted to string 
+                            Console.WriteLine(MethodBase.GetCurrentMethod().Name, ex);
                         }
-                        //FWAppObj.FWHost.WriteToDebugLogger(DebugModeType.DEBUG, "WriteFwAPIRawData " + byteArr);//the final byte is printed in Debug logger
-                        // objHelperComm.m_CommonAPIcontroller.WriteReg(inputBytes.ToArray(), $"Index: {apiRawData.CurrIndex}");
-                        Console.WriteLine(BitConverter.ToString(inputBytes.ToArray()), $"Index: {apiRawData.CurrIndex}");
-                        apiRawData.CurrIndex += 1;
-                        Thread.Sleep(Delay);//Wait for given delay
-                        string status = "Write Success";
-                        string resultStatus = error ? "Contains Error" : status;
-                        apiRawData.fwResponse = resultStatus; // the status whether it contains error or succcess will print in UI
                     }
-                    else
+                    foreach (byte byteValue in inputBytes)
                     {
-                        CheckValidInputAndSendToBE(apiString, apiRawData);//if the hex checkbox was not checked this method will execute
+                        byteArr += (byteValue.ToString() + " ");// all the bytes are comverted to string 
                     }
+                    //FWAppObj.FWHost.WriteToDebugLogger(DebugModeType.DEBUG, "WriteFwAPIRawData " + byteArr);//the final byte is printed in Debug logger
+                    // objHelperComm.m_CommonAPIcontroller.WriteReg(inputBytes.ToArray(), $"Index: {apiRawData.CurrIndex}");
+                    Console.WriteLine(BitConverter.ToString(inputBytes.ToArray()), $"Index: {apiRawData.CurrIndex}");
+                    apiRawData.CurrIndex += 1;
+                    Thread.Sleep(Delay);//Wait for given delay
+                    string status = "Write Success";
+                    string resultStatus = error ? "Contains Error" : status;
+                    apiRawData.fwResponse = resultStatus; // the status whether it contains error or succcess will print in UI
+                    //}
+                    //else
+                    //{
+                    //    //CheckValidInputAndSendToBE(apiString, apiRawData);//if the hex checkbox was not checked this method will execute
+                    //}
                 }
             }
             catch (Exception ex)
@@ -90,7 +112,7 @@ namespace CustomAPI
             }
 
         }
-        private static void CheckValidInputAndSendToBE(string eachInputCmds, ExerFwAPIWrite apiRawData)
+      /*  private static void CheckValidInputAndSendToBE(string eachInputCmds, ExerFwAPIWrite apiRawData)
         {
             bool error = false; //initially no errors
             string Input = eachInputCmds;
@@ -158,6 +180,6 @@ namespace CustomAPI
             string status = "Write Success";
             string resultStatus = error ? "Contains Error" : status;
             apiRawData.fwResponse = resultStatus; // the status whether it contains error or succcess will print in UI
-        }
+        }*/
     }
 }
