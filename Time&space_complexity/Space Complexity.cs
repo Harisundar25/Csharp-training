b9ec2d140d266d9a504d6830a1a1d4e3218609a4
HariSundar-GRL <harisundar@graniteriverlabs.in>
2024-08-23 11:48:16 +0530

    time and space complexity

---
+++ Time&space_complexity/Space Complexity.cs
@@ -0,0 +1,53 @@
+﻿namespace Time_space_complexity
+{
+    public class Space_Complexity
+    {
+        /// <summary>
+        /// Constant Space - O(1)
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int AddTwoNumbers(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        /// <summary>
+        /// Linear Space - O(n)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int[] CreateArray(int n)
+        {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = i;
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
+            return arr;
+        }
+        /// <summary>
+        ///  Quadratic Space - O(n^2)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int[,] CreateMatrix(int n)
+        {
+            int[,] matrix = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = i * j;
+                    Console.WriteLine(matrix[i,j]);
+                }
+            }
+            Console.ReadKey();
+            return matrix;
+        }
+
+    }
+}
---
+++ Time&space_complexity/Time complexity.cs
@@ -0,0 +1,46 @@
+﻿namespace Time_space_complexity
+{
+    public class Time_complexity
+    {
+        /// <summary>
+        /// Method represents Constant Time - O(1)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int GetFirstElement(int[] arr)
+        {
+            return arr[4];
+        }
+        /// <summary>
+        /// Linear Time - O(n)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int SumArray(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        /// <summary>
+        /// Quadratic Time - O(n^2)
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int CountPairs(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
